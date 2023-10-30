02ffa62104fb461ccc690a8c6983b8df8cca4e91
Verners-Skrastis <verners.skrastins@grobinasskola.lv>
2023-10-30 15:23:48 +0200

    ...

--- 1uzd.py
+++ 1uzd.py
@@ -2,4 +2,34 @@
 Uzrakstiet programmu definējot klasi,lai veselu skaitli pārveidotu par romiešu ciparu.
 
 """
+class RomiesuCipars:
+    def __init__(self):
+        self.rom_cip = {
+            1:"I",
+            4:"IV",
+            5:"V",
+            9:"IX",
+            10:"X",
+            40:"XL",
+            50:"L",
+            90:"XC",
+            100:"C",
+            400:"CD",
+            500:"D",
+            900:"CM",
+            1000:"M"
 
+        }
+    def uzRomiesu(self, num):
+        result = ""
+        for value, sk in sorted(self.rom_cip.items(), key = lambda x:x[0], reverse=True):
+            while num>=value:
+                result+=sk
+                num-=value
+        return result
+
+
+skaitlis = 654
+klase = RomiesuCipars()
+romu_sks = klase.uzRomiesu(skaitlis)   
+print(romu_sks)     
