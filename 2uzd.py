39af220ab9a86aeffeadbf3d087adf7b00c8e527
Verners-Skrastis <verners.skrastins@grobinasskola.lv>
2023-10-30 15:47:46 +0200

    var

--- 2uzd.py
+++ 2uzd.py
@@ -3,3 +3,19 @@ Iegūt ziņas, izmantojot rss no google.lv.
 
 """
 
+import feedparser
+
+
+# jānorāda RSS plūsmas URL
+rss_URL="https://news.google.com/rss"
+
+
+#lejupialadet un izanalizet RSS plūsmu
+feed=feedparser.parse(rss_URL)
+
+#paradit zinu virsrakstus un saites
+
+for entry in feed.entries:
+    print('virsraksts:', entry.title)
+    print("Saite", entry.link)
+    print("\n")
